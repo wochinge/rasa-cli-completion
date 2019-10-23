839d773e39ff7b4b317f3e08ecd4b14ab1fc30bf
Tobias Wochinger <t.wochinger@rasa.com>
2019-10-23 22:06:01 +0200

    print correct bash / zsh config

--- rasa_cli_completion/show_config.py
+++ rasa_cli_completion/show_config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 if __name__ == "__main__":
-    script_name = "rasa-complete.sh"
-    script_location = Path(__file__).parent / script_name
+    install_directory = Path(__file__).parent
+    shell_script_location = install_directory / "rasa-complete.sh"
+    py_script_location = install_directory / "rasa_complete.py"
 
     print("Thanks for installing this library 🚀")
     print("Add this to your bash / zsh config:\n ")
-    print(f"EXPORT RASA_COMPLETE_SCRIPT={script_location}")
+    print(f"source {shell_script_location}")
+    print(f"export RASA_COMPLETE_SCRIPT={py_script_location}")
